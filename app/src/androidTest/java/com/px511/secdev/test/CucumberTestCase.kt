c6fef306e117b731e5b66941106a36f5a7570e2a
Gabillet <theogabillet@users.noreply.github.com>
2024-01-08 01:33:00 +0100

    removed tags options from CucumberTestCase to simplify the use of new tags

--- app/src/androidTest/java/com/px511/secdev/test/CucumberTestCase.kt
+++ app/src/androidTest/java/com/px511/secdev/test/CucumberTestCase.kt
@@ -3,7 +3,6 @@ package com.px511.secdev.test
 import cucumber.api.CucumberOptions
 
 @CucumberOptions(features = ["features"],
-        glue = ["com.px511.secdev.cucumber.steps"],
-        tags = ["@e2e", "@smoke"])
+        glue = ["com.px511.secdev.cucumber.steps"])
 @Suppress("unused")
 class CucumberTestCase
